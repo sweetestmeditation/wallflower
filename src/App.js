88aad27009aaac162178c804aab3c1b3fd217c11
bngwanforbi <brendan.ngwanforbi@gmail.com>
2023-09-15 05:41:43 -0700

    Intergrate React Router into App.js, adding routes for the Dating and ProfileCard cmponents, and setting up the default route for chat functionality.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
 import "./App.css";
 import MainContainer from "./Components/MainContainer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dating from "./Components/Dating"; // Import the Dating component
+import ProfileCard from "./Components/ProfileCard"; // Import the ProfileCard component
 
 function App() {
   return (
-    <div className="Wallflower">
-   
-      <MainContainer />
-    </div>
+    <Router>
+      <Routes>
+        {/* Route for the dating page */}
+        <Route path="/dating" element={<Dating />} />
+
+        {/* Route for the profile card */}
+        <Route path="/profile" element={<ProfileCard />} />
+
+        {/* Default route (for chat functionality) */}
+        <Route path="/" element={<MainContainer />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
+
